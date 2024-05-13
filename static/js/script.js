1cead9805d1ead6a51c71438fe384aa45d65f387
nurali-code <nur3.dav.97@gmail.com>
2024-05-13 15:15:18 +0500

    new fix

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,10 @@
+const menuToggler = document.getElementById("menuToggler");
+const menu = document.getElementById("menu");
+menuToggler.onclick = (event) => {
+    menuToggler.classList.toggle('active');
+    menu.classList.toggle('Menu_container__puOAc');
+    menu.classList.toggle('Menu_containerOpen__MJuw7');
+}
 
 const contractAbi = [
     { inputs: [], stateMutability: "nonpayable", type: "constructor" },
