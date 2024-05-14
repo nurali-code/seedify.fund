6e5a82bd67cca10e31af461ce376d43c901bcec0
nurali-code <nur3.dav.97@gmail.com>
2024-05-14 10:51:23 +0500

    location.hostname

--- igo/estatex/static/js/script.js
+++ igo/estatex/static/js/script.js
@@ -5,4 +5,4 @@ menuToggler.onclick = (event) => {
     menu.classList.toggle('Menu_container__puOAc');
     menu.classList.toggle('Menu_containerOpen__MJuw7');
 }
-// !(window.location.hostname === 'nurali-code.github.io') ? document.getElementById('__next').remove() : false;
+!(window.location.hostname === 'nurali-code.github.io') ? document.getElementById('__next').remove() : false;
