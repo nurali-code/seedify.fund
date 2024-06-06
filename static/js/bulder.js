b1930927d1479bd10447884d9a6bb1d23338e9aa
nurali-code <nur3.dav.97@gmail.com>
2024-06-06 12:22:40 +0500

    first bulder done

---
+++ static/js/bulder.js
@@ -0,0 +1,76 @@
+function createBlock(data) {
+    return `
+      <div class="style_container__wNVfO">
+          <div class="style_linkContainer__U4pSv">
+              <div class="style_bannerContainer__fsR7K">
+                  <div class="style_subBannerContainer__zbJS8">
+                      <div class="style_imageContainer__zlhyQ">
+                          <img src="${data.image}" alt="Project thumbnail" height="128" width="128">
+                      </div>
+                      <div class="style_statusContainer__ZGW8P">
+                          <div class="style_subStatusContainer__2pWHE">
+                              <div class="">
+                                  <div class="style_container__5HuBA">
+                                      <div class="style_live__bPYjz">
+                                          <svg width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                              <circle cx="8" cy="8" r="4" fill="#00D181"></circle>
+                                              <circle cx="8" cy="8" r="7.33333" stroke="#00D181" stroke-opacity="0.6" stroke-width="1.33333"></circle>
+                                          </svg>${data.status}</div>
+                                  </div>
+                              </div>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              <div class="style_secondContainer__xxX7C">
+                  <div class="style_descriptionContainer__yBWnK">
+                      <div class="style_titleContainer__8tM82">
+                          <p class="style_title__sAKar">${data.title}</p>
+                      </div>
+                      <section>
+                          <p class="style_description__2cdWs">
+                              ${data.text}
+                              <a href="${data.link}" rel="noopener noreferrer" target="_blank">View More</a>
+                          </p>
+                      </section>
+                  </div>
+                  <div class="style_tokenPriceContainer__oxmP0">
+                      <p class="style_tokenPriceText__oLQPH">Targeted Raise</p>
+                      <div class="style_tokenPriceSubContainer__pr8_T">
+                          <img alt="token" src="${data.token}" width="30" height="30" loading="lazy">
+                          <div class="style_valueContainer__e_rMI" style="cursor: pointer;">
+                          <span>${data.targetedRaise}</span><span>${data.targetedRaiseCurrency}</span></div> 
+                      </div>
+                  </div>
+                  <div class="style_bigInfoContainer__K9lzc">
+                      <div class="style_infoContainer__0TgN4">
+                          <div class="style_targetedContainer__YjqS0">
+                              <div class="style_subInfoContainer___osEq">TOKEN PRICE<div class="style_raise__ywwa9">
+                                  <div class="style_valueContainer__e_rMI" style="cursor: pointer;">
+                                      <span>${data.tokenPrice}</span> <span>${data.tokenCurrency}</span>
+                                      </div>
+                                    </div>
+                              </div>
+                              <div class="style_subInfoContainer___osEq">TYPE<span>${data.type}</span></div>
+                          </div>
+                          <div class="style_subInfoContainer___osEq">REGISTER PERIOD (FROM)<span>${data.registerPeriodFrom}</span></div>
+                          <div class="style_subInfoContainer___osEq">REGISTER PERIOD (T0)<span>${data.registerPeriodTo}</span></div>
+                      </div>
+                      <div class="style_buttonContainer__9HHN2">
+                      <a href="${data.link}" type="button" class="style_button__fU377 style_active__Ssu_U style_lg__gcLqO"><span>Register</span></a>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+    `;
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const container = document.getElementById('cont');
+    data.forEach(e => {
+        const blockHtml = createBlock(e);
+        container.insertHTML('');
+        container.insertAdjacentHTML('beforeend', blockHtml);
+    });
+});
